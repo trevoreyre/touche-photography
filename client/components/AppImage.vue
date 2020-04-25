16abdcb9eec5aee6dda024220aaa3aa5af97f4f5
Trevor Eyre <trevoreyre@gmail.com>
2020-04-24 23:18:48 -0600

    Updated netlify dev setup. Fixed reference in AppImage component.

--- client/components/AppImage.vue
+++ client/components/AppImage.vue
@@ -1,84 +1,81 @@
 <script>
-import get from 'lodash/get'
-import sanityImageUrl from "@sanity/image-url";
-import sanity from "~/sanity";
-import { marginMixin } from '@slate-ui/core'
+  import get from 'lodash/get'
+  import sanityImageUrl from '@sanity/image-url'
+  import sanityClient from '~/sanityClient'
+  import { marginMixin } from '@slate-ui/core'
 
-const urlBuilder = sanityImageUrl(sanity);
+  const urlBuilder = sanityImageUrl(sanityClient)
 
-export default {
-  mixins: [marginMixin],
-  props: {
-    image: {
-      type: Object,
-      required: true
+  export default {
+    mixins: [marginMixin],
+    props: {
+      image: {
+        type: Object,
+        required: true,
+      },
+      src: String,
+      alt: String,
+      width: {
+        type: Number,
+        default: 400,
+      },
+      height: {
+        type: Number,
+      },
     },
-    src: String,
-    alt: String,
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-    }
-  },
 
-  computed: {
-    imageUrl() {
-      return urlBuilder
-        .image(this.image)
-        .width(this.width)
-        .height(this.height)
-        .url();
-    },
-    placeholder() {
-      return get(this.image, 'asset.metadata.lqip')
-    },
-    aspectRatio() {
-      return get(this.image, 'asset.metadata.dimensions.aspectRatio', 1)
-    },
-    style() {
-      return {
-        paddingBottom: `${(1 / this.aspectRatio) * 100}%`
-      }
+    computed: {
+      imageUrl() {
+        return urlBuilder
+          .image(this.image)
+          .width(this.width)
+          .height(this.height)
+          .url()
+      },
+      placeholder() {
+        return get(this.image, 'asset.metadata.lqip')
+      },
+      aspectRatio() {
+        return get(this.image, 'asset.metadata.dimensions.aspectRatio', 1)
+      },
+      style() {
+        return {
+          paddingBottom: `${(1 / this.aspectRatio) * 100}%`,
+        }
+      },
     },
   }
-};
 </script>
 
 <template>
-  <div
-    :class="[$style.appImage, marginClass]"
-    :style="style"
-  >
-    <img :class="$style.placeholder" :src="placeholder">
-    <img :class="$style.image" :src="imageUrl" :alt="alt">
+  <div :class="[$style.appImage, marginClass]" :style="style">
+    <img :class="$style.placeholder" :src="placeholder" />
+    <img :class="$style.image" :src="imageUrl" :alt="alt" />
   </div>
 </template>
 
 <style module>
-.app-image {
-  border-radius: var(--border-radius-default);
-  height: 0;
-  overflow: hidden;
-}
+  .app-image {
+    border-radius: var(--border-radius-default);
+    height: 0;
+    overflow: hidden;
+  }
 
-.placeholder {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  filter: blur(8px);
-  transform: scale(1.1);
-}
+  .placeholder {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    filter: blur(8px);
+    transform: scale(1.1);
+  }
 
-.image {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
+  .image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 </style>
--- client/functions/get-shipping/get-shipping.js
+++ client/functions/get-shipping/get-shipping.js
@@ -7,9 +7,14 @@ const sanityClient = SanityClient({
   useCdn: true,
 })
 
+const SHIPPING_METHODS_QUERY = `
+  *[_type == 'shippingMethod' && product._ref == $productId]
+`
+// && $sizeKey in rates[].sizes
+
 exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
-  console.log(util.inspect(body, false, null, true))
+  // console.log(util.inspect(body, false, null, true))
 
   const { eventName, content } = body
   if (eventName !== 'shippingrates.fetch') {
@@ -20,27 +25,50 @@ exports.handler = async (event, context, callback) => {
   }
 
   const { items } = content
+  const shippingMethodResponses = await Promise.all(
+    content.items.map((item) => {
+      const { productId, sizeKey } = item.metadata
+      console.log('productId:', productId)
+      return sanityClient.fetch(SHIPPING_METHODS_QUERY, { productId, sizeKey })
+    })
+  )
+
+  const ratesByItem = items.map((item, index) => {
+    const { sizeKey } = item.metadata
+    return shippingMethodResponses[index].map((shippingMethod) => {
+      console.log(
+        'shippingMethod',
+        util.inspect(shippingMethod, false, null, true)
+      )
+      const [rate] = shippingMethod.rates.filter((rate) =>
+        rate.sizes.includes(sizeKey)
+      )
+      return {
+        description: shippingMethod.name,
+        guaranteedDaysToDelivery: shippingMethod.deliveryTime,
+        cost: rate.rate * item.quantity,
+      }
+    })
+  })
+  console.log('ratesByItem', util.inspect(ratesByItem, false, null, true))
+
+  const rates = ratesByItem.reduce((rates, itemRates) => {
+    itemRates.forEach((rate) => {
+      if (rates[rate.description]) {
+        rates[rate.description].cost += rate.cost
+      } else {
+        rates[rate.description] = rate
+      }
+    })
+    return rates
+  }, {})
+  console.log('rates', util.inspect(rates, false, null, true))
 
   return callback(null, {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      rates: [
-        {
-          cost: 10,
-          description: 'Ground shipping',
-        },
-        {
-          cost: 20,
-          description: 'Two day shipping',
-          guaranteedDaysToDelivery: 2,
-        },
-        {
-          cost: 30,
-          description: 'Overnight shipping',
-          guaranteedDaysToDelivery: 1,
-        },
-      ],
+      rates: Object.values(rates),
     }),
   })
   {
