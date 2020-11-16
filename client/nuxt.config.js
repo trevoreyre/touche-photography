67f83beaae22c4649a7ec858465b7102ebefc5ff
Trevor Eyre <trevoreyre@gmail.com>
2020-11-15 22:09:10 -0800

    Added favicon (#57)

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -11,14 +11,35 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#009c8d' },
     ],
-    // Snipcart styling. JavaScript file is included in default layout.
     link: [
+      // Snipcart styling. JavaScript file is included in default layout.
       {
-        href: 'https://cdn.snipcart.com/themes/v3.0.11/default/snipcart.css',
-        type: 'text/css',
         rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://cdn.snipcart.com/themes/v3.0.11/default/snipcart.css',
       },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#009c8d' },
     ],
   },
   router: {
